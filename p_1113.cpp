c9af4bd21a4822fe05ebde143dbd97228bc78de5
Sourav Bosu <121472055+Sou963@users.noreply.github.com>
2026-01-02 01:57:20 +0600

    Add files via upload

---
+++ p_1113.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int X, Y;
+    while (true) {
+        cin >> X >> Y;
+        if (X == Y)
+            break;
+        if (X < Y)
+            cout << "Crescente" << endl;
+        else
+            cout << "Decrescente" << endl;
+    }
+    return 0;
+}
+
